cc112e0099189656213197943c42eaca7936f284
wafkaito <134695080+wafkaito@users.noreply.github.com>
2023-07-25 15:29:01 +0700

    Update SUSCO.py

--- SUSCO.py
+++ SUSCO.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class Test1():
   def setup_method(self, method):
-        self.driver = webdriver.Remote(command_executor='http://192.168.1.39:4444/', desired_capabilities=DesiredCapabilities)
+        self.driver = webdriver.Remote(command_executor='http://10.99.20.131:4444/', desired_capabilities=DesiredCapabilities.Chrome)
         self.driver.maximize_window()
         self.vars = {}
   
